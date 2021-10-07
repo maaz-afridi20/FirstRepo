f5835d3f1702ac622093ee648f79eeaacd187c95
maaz-afridi20 <maazafridi.ma47@gmail.com>
2021-10-07 15:48:18 +0500

    5 lines added

--- testing.r
+++ testing.r
@@ -2,3 +2,9 @@
 print("hello git")
 print("this is version control topic")
 print("this is first git commit file. ")
+
+name = "maazAfridi"
+versionn = "Rstudion-1.1"
+
+print(name)
+print("these line of code is from R studio")

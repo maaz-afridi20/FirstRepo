12880c389dc4b84e97402f7f041460e46b37b3a6
maaz-afridi20 <maazafridi.ma47@gmail.com>
2021-10-13 18:24:40 +0500

    data manupulation through Filter Function

--- datamanupulation.R
+++ datamanupulation.R
@@ -24,3 +24,39 @@ View(laptop %>% select(starts_with("P"))) # column that start with P
 
 View(laptop %>% select(ends_with("y"))) # this will show column name
 # that ends with y
+
+
+# ----------------------------------------------------
+
+# Filter :
+
+laptop_Rams = laptop %>% filter(RAM == "8GB")
+View(laptop_Rams)
+# this will show only that laptops that have 8gb of rams
+
+company = laptop %>% filter(Manufacturer == "Apple")
+View(company)
+# will only show laptops of Apple 
+
+
+lapWeight = laptop %>% filter(Weight <= "1.00kg")
+View(lapWeight)
+# will show only laptops having weight less than 1 kg. 
+
+cofiguration = laptop %>% filter(
+    Manufacturer == "Dell" & Screen.Size <= 15)
+View(cofiguration)
+
+
+View(laptop %>% select("Manufacturer") %>% filter("Category"=="Notebook"))
+
+quality = laptop %>% filter(Manufacturer == "Apple")
+View(quality)
+
+
+
+
+
+
+
+
